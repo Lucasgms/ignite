13ea11dc11315c9ef150fef5e5f36707c607c95b
Lucasgms <lgregory12@hotmail.com>
2021-06-23 11:30:40 -0300

    feat: [dtmoney] new transaction modal
    implement income and outcome button functionality and style

--- 02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -12,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal
       isOpen={isOpen}
@@ -26,6 +28,7 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
+
       <Container>
         <h2>Cadastrar transação</h2>
 
@@ -33,15 +36,25 @@ export function NewTransactionModal ({ isOpen, onRequestClose }: NewTransactionM
         <input type="number" placeholder="Valor" />
 
         <TransactionTypeContainer>
-          <button type="button">
+          <RadioBox
+            type="button"
+            isActive={type === 'deposit'}
+            activeColor='green'
+            onClick={() => setType('deposit')}
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button type="button">
+          <RadioBox
+            type="button"
+            isActive={type === 'withdraw'}
+            activeColor='red'
+            onClick={() => setType('withdraw')}
+          >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input type="text" placeholder="Categoria" />
