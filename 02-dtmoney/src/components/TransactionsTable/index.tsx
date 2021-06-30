8f72ef678fa237d60ba542f857ef02ea5a3d8ddb
Lucasgms <lgregory12@hotmail.com>
2021-06-30 09:58:02 -0300

    feat: [dtmoney] list transactions
    implement transactions dynamic list

--- 02-dtmoney/src/components/TransactionsTable/index.tsx
+++ 02-dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  category: string;
+  type: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get('transactions').then(response => console.log(response.data));
+    api.get('transactions').then(response => setTransactions(response.data.transactions));
   }, []);
+
   return (
     <Container>
       <table>
@@ -18,18 +30,14 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Item 1</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Item 2</td>
-            <td className="withdraw">- R$2.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>R${transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
